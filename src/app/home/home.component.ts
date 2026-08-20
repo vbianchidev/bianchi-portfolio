2a85f0364385d9febc1a1713d0305ec2ebeefec2
vbianchi.dev <vitor.biansil@gmail.com>
2026-08-20 07:45:00 -0300

    crete home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  imports: [BuiTypewriter],
+})
+export class HomeComponent {}
