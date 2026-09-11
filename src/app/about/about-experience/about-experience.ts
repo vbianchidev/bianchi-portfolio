82d0a4e6a18e81e44219902e3d455fd008081710
vbianchi.dev <vitor.biansil@gmail.com>
2026-09-11 04:05:21 -0300

    SYNC

---
+++ src/app/about/about-experience/about-experience.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-experience',
+  templateUrl: './about-experience.html',
+  styleUrls: ['./about-experience.scss'],
+})
+export class AboutExperience {}
--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AboutDisplay } from './about-display/about-display';
+import { AboutExperience } from './about-experience/about-experience';
 import { AboutMe } from './about-me/about-me';
 import { AboutSkills } from './about-skills/about-skills';
 
 @Component({
   templateUrl: './about.html',
   styleUrl: './about.scss',
-  imports: [AboutDisplay, AboutMe, AboutSkills],
+  imports: [AboutDisplay, AboutMe, AboutSkills, AboutExperience],
 })
 export class About {}
