8a34eee8d36604b75a44b665e24da7ca370d996d
vbianchi.dev <vitor.biansil@gmail.com>
2026-09-11 10:38:53 -0300

    sync

--- src/app/about/about-experience/about-experience.ts
+++ src/app/about/about-experience/about-experience.ts
@@ -1,8 +1,52 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { BuiAccordion, BuiAccordionItem } from '@bianchi-ui';
+import { TranslatePipe } from '@ngx-translate/core';
+import { SectionTitleComponent } from '../../@common/components/section-title/section-title.component';
+
+export interface Experience {
+  company: string;
+  role: string;
+  startDate: Date;
+  endDate: Date;
+  description: string;
+}
 
 @Component({
   selector: 'app-about-experience',
   templateUrl: './about-experience.html',
   styleUrls: ['./about-experience.scss'],
+  imports: [TranslatePipe, DatePipe, SectionTitleComponent, BuiAccordion, BuiAccordionItem],
 })
-export class AboutExperience {}
+export class AboutExperience {
+  experiences: Experience[] = [
+    {
+      company: 'Selbetti Tecnologia',
+      role: 'Frontend Developer (Angular)',
+      startDate: new Date('2024-01-01'),
+      endDate: new Date('2024-12-31'),
+      description: '',
+    },
+    {
+      company: 'Informa Solutions',
+      role: 'Frontend Developer (Angular)',
+      startDate: new Date('2023-01-01'),
+      endDate: new Date('2024-12-31'),
+      description: '',
+    },
+    {
+      company: 'Integrativa',
+      role: 'Fullstack Developer (.NET Core / Angular)',
+      startDate: new Date('2022-01-01'),
+      endDate: new Date('2024-12-31'),
+      description: '',
+    },
+    {
+      company: 'Maquinaweb',
+      role: 'Fullstack Developer (PHP)',
+      startDate: new Date('2022-01-01'),
+      endDate: new Date('2024-12-31'),
+      description: '',
+    },
+  ];
+}
